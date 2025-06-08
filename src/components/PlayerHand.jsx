619c7373e52e8c76b09617dc69ba1d381a27ef4c
florian-celibashi <floriantoo@icloud.com>
2025-06-08 09:25:02 -0400

    Updated UI for player's hand.

--- src/components/PlayerHand.jsx
+++ src/components/PlayerHand.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 function PlayerHand({ hand }) {
     return (
-        <div className="player-hand">
-            <h2>Your Hand:</h2>
-            <div className="cards">
+        <div className="player-hand fixed bottom-18 left-1/2 transform -translate-x-1/2 h-48 w-auto flex flex-col items-center">
+            <h2 className="text-xl font-bold mb-2">Your Hand:</h2>
+            <div className="cards flex flex-row-reverse justify-center items-center space-x-reverse space-x-2 h-full">
                 {hand.map((card, index) => {
                     const filename = `${card.rank}_of_${card.suit}.svg`
                     const imagePath = `/assets/svg-cards/${filename}`
                     return (
                         <div key={index} className="card">
-                            <img src={imagePath} alt={`${card.rank} of ${card.suit}`} />
+                            <img src={imagePath} alt={`${card.rank} of ${card.suit}`} className="w-36 h-auto" />
                         </div>
                     )
                 })}

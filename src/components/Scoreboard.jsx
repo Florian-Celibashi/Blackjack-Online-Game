8cda268897c43f819acd1cd80d798b48b17ae549
florian-celibashi <floriantoo@icloud.com>
2025-06-09 21:46:35 -0400

    Tweaked Scoreboard UI

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,6 +1,6 @@
 function Scoreboard({ wins, losses, streak }) {
     return (
-        <div className="fixed top-4 right-4 bg-black/60 rounded-xl px-4 py-2 text-sm lg:text-base">
+        <div className="fixed left-12 bottom-30 bg-black/60 rounded-xl px-4 py-2 text-sm lg:text-base">
             <p>Wins: {wins}</p>
             <p>Losses: {losses}</p>
             <p>Streak: {streak}</p>
